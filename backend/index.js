09260a4291326b3aee205cfca7c6c38725956d38
git-blank993 <manishsuresh993@gmail.com>
2026-06-06 16:11:04 +0530

    feat: setup a very simple req logger

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,22 @@ import express from "express";
 import Config from "./config/config.js";
 import healthRouter from "./controllers/health/index.js";
 import authRouter from "./controllers/auth/index.js";
+import { JSONReturn } from "./utils/index.js";
 const app = express();
 const config = Config;
 const port = config.getPort();
 
+app.use((req, _res) => {
+  console.log(`${req.method} -> ${req.url}`);
+});
 app.use(express.json());
 app.use("/health", healthRouter);
 app.use("/auth", authRouter);
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+const stats = new JSONReturn("Tracker API live", true);
+
+app.get("/", (_req, res) => {
+  res.json(stats);
 });
 
 app.listen(port, () => {
